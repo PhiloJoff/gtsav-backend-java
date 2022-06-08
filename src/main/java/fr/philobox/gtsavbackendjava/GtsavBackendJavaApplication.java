9c7391f47f8caeba17787ccb8b78b98d26d8921f
philojoff <1279976+PhiloJoff@users.noreply.github.com>
2022-06-08 13:26:03 +0200

    init commit

---
+++ src/main/java/fr/philobox/gtsavbackendjava/GtsavBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package fr.philobox.gtsavbackendjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtsavBackendJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GtsavBackendJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/philobox/gtsavbackendjava/GtsavBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.philobox.gtsavbackendjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtsavBackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
